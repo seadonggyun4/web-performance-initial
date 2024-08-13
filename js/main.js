ce898150b9e87eaf9735bcdbcabf472f5be315f3
seadonggyun4 <seadonggyun@gmail.com>
2024-08-13 20:39:47 +0900

    fix: displayProducts 코드 개선

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-(function showTopBar() {
-    const $countryBar = document.querySelector("section.country-bar")
-    const COUNTRY = "France";
-    const VAT = 20;
+document.addEventListener('DOMContentLoaded', (event) => {
+    (function showTopBar() {
+        const $countryBar = document.querySelector("section.country-bar")
+        const COUNTRY = "France";
+        const VAT = 20;
 
-    setTimeout(() => {
-        $countryBar.innerHTML = `<p>Orders to <b>${COUNTRY}</b> are subject to <b>${VAT}%</b> VAT</p>`
-        $countryBar.classList.remove('hidden')
-    }, 1000);
-})();
+        setTimeout(() => {
+            $countryBar.innerHTML = `<p>Orders to <b>${COUNTRY}</b> are subject to <b>${VAT}%</b> VAT</p>`
+            $countryBar.classList.remove('hidden')
+        }, 1000);
+    })();
+});
